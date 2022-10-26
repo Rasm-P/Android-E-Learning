48dbc30aa889edc304dd803f999728d6f22145ff
Rasm-P <rasmus.pra@gmail.com>
2022-10-27 00:10:28 +0200

    Added di modules and application for dependancy injection using Dagger Hilt

--- ELearningApp/app/src/main/java/com/example/elearningapp/di/ServiceModule.kt
+++ ELearningApp/app/src/main/java/com/example/elearningapp/di/ServiceModule.kt
@@ -1,6 +1,19 @@
 package com.example.elearningapp.di
 
+import com.example.elearningapp.repositories.LoginRepository
+import com.example.elearningapp.repositories.LoginRepositoryInterface
 import dagger.Module
+import dagger.Binds
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ViewModelComponent
 
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class AppServiceModule {
+
+    @Binds
+    abstract fun bindLoginRepository(
+        loginRepository: LoginRepository
+    ): LoginRepositoryInterface
+
+}
