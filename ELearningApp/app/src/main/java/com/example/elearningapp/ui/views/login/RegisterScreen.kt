b737292329f506184439b724a640d2641406574d
Rasm-P <rasmus.pra@gmail.com>
2022-10-31 22:59:41 +0100

    Modified the RegisterScreen composable layout

--- ELearningApp/app/src/main/java/com/example/elearningapp/ui/views/login/RegisterScreen.kt
+++ ELearningApp/app/src/main/java/com/example/elearningapp/ui/views/login/RegisterScreen.kt
@@ -6,8 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Error
-import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,24 +25,19 @@ import com.example.elearningapp.R
 fun RegisterScreen(navigateLogin: () -> Unit) {
     val image: Painter = painterResource(id = R.drawable.e_learning)
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Column {
+        Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top) {
+            Image(
+                modifier = Modifier
+                    .weight(1f)
+                    .size(128.dp),
+                painter = image,
+                contentDescription = "App Logo"
+            )
             Box(modifier = Modifier
-                .weight(0.6f)
-                .fillMaxWidth(), contentAlignment = Alignment.Center) {
-                Image(
-                    modifier = Modifier.fillMaxHeight(0.6f),
-                    painter = image,
-                    contentDescription = "App Logo"
-                )
-            }
-            Box(modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth(), contentAlignment = Alignment.BottomCenter) {
+                .weight(2f)) {
                 RegisterCard(navigateLogin)
             }
         }
-    }
 }
 
 @Composable
@@ -54,112 +48,111 @@ fun RegisterCard(navigateLogin: () -> Unit) {
     var passwordVisible by remember { mutableStateOf(false) }
     var registerFailed by remember { mutableStateOf(false) }
 
-    Card(modifier = Modifier.fillMaxSize(),
-        shape = RoundedCornerShape(20.dp, 20.dp),
+    Card(shape = RoundedCornerShape(20.dp, 20.dp),
         elevation = 12.dp) {
-        Box(modifier = Modifier.padding(30.dp)) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp)) {
+            Text(
+                text = "Sign Up",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Medium
+            )
             Column {
-                Box(modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = Alignment.TopCenter) {
-                    Column {
-                        Text(
-                            text = "Sign Up",
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Medium
-                        )
-                        Spacer(modifier = Modifier.height(12.dp))
-                        Text(
-                            text = "Email",
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Light
-                        )
-                        OutlinedTextField(modifier = Modifier
-                            .fillMaxWidth(),
-                            value = email,
-                            onValueChange = {email = it},
-                            singleLine = true,
-                            trailingIcon = {
-                                if (registerFailed)
-                                    Icon(Icons.Filled.Error,"Login error", tint = MaterialTheme.colors.error)
+                Spacer(modifier = Modifier.height(12.dp))
+                OutlinedTextField(modifier = Modifier
+                    .fillMaxWidth(),
+                    value = email,
+                    label = { Text(text = "Email") },
+                    onValueChange = {email = it},
+                    singleLine = true,
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            email = ""
+                        }) {
+                            if (email != "") {
+                                Icon(Icons.Filled.Clear, "Clear email")
                             }
-                        )
-                        Spacer(modifier = Modifier.height(12.dp))
-                        Text(
-                            text = "Password",
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Light
-                        )
-                        OutlinedTextField(modifier = Modifier
-                            .fillMaxWidth(),
-                            value = password,
-                            onValueChange = {password = it},
-                            singleLine = true,
-                            trailingIcon = {
-                                IconButton(onClick = {
-                                    passwordVisible = !passwordVisible
-                                }) {
-                                    Icon(Icons.Filled.Visibility, "Password visibility")
-                                }
-                            },
-                            visualTransformation = if (passwordVisible) VisualTransformation.None
-                            else PasswordVisualTransformation()
-                        )
-                        Spacer(modifier = Modifier.height(12.dp))
-                        Text(
-                            text = "Repeat Password",
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.Light
-                        )
-                        OutlinedTextField(modifier = Modifier
-                            .fillMaxWidth(),
-                            value = repeatPassword,
-                            onValueChange = {repeatPassword = it},
-                            singleLine = true,
-                            trailingIcon = {
-                                IconButton(onClick = {
-                                    passwordVisible = !passwordVisible
-                                }) {
-                                    Icon(Icons.Filled.Visibility, "Password visibility")
-                                }
-                            },
-                            visualTransformation = if (passwordVisible) VisualTransformation.None
-                            else PasswordVisualTransformation()
-                        )
-                        if (registerFailed) {
-                            Text(
-                                text = "Wrong email or password!",
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Light,
-                                color = MaterialTheme.colors.error,
-                                style = MaterialTheme.typography.caption,
-                                modifier = Modifier.align(Alignment.Start)
-                            )
-                            Spacer(modifier = Modifier.height(10.dp))
-                        } else {
-                            Spacer(modifier = Modifier.height(32.dp))
-                        }
-                        Button(modifier = Modifier
-                            .fillMaxWidth()
-                            .height(40.dp),
-                            onClick = { /*TODO*/ }) {
-                            Text(text = "SIGN UP")
                         }
                     }
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                OutlinedTextField(modifier = Modifier
+                    .fillMaxWidth(),
+                    value = password,
+                    label = { Text(text = "Password") },
+                    onValueChange = {password = it},
+                    singleLine = true,
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            passwordVisible = !passwordVisible
+                        }) {
+                            if (passwordVisible) {
+                                Icon(Icons.Filled.Visibility, "Password visible")
+                            } else {
+                                Icon(Icons.Filled.VisibilityOff, "Password not visible")
+                            }
+                        }
+                    },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None
+                    else PasswordVisualTransformation()
+                )
+                Spacer(modifier = Modifier.height(12.dp))
+                OutlinedTextField(modifier = Modifier
+                    .fillMaxWidth(),
+                    value = repeatPassword,
+                    label = { Text(text = "Repeat Password") },
+                    onValueChange = {repeatPassword = it},
+                    singleLine = true,
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            passwordVisible = !passwordVisible
+                        }) {
+                            if (passwordVisible) {
+                                Icon(Icons.Filled.Visibility, "Password visible")
+                            } else {
+                                Icon(Icons.Filled.VisibilityOff, "Password not visible")
+                            }
+                        }
+                    },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None
+                    else PasswordVisualTransformation()
+                )
+                if (registerFailed) {
+                    Text(
+                        text = "Wrong email or password!",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Light,
+                        color = MaterialTheme.colors.error,
+                        style = MaterialTheme.typography.caption,
+                        modifier = Modifier.align(Alignment.Start)
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
+                } else {
+                    Spacer(modifier = Modifier.height(32.dp))
                 }
-                Box(modifier = Modifier
-                    .fillMaxWidth().fillMaxHeight(),
-                    contentAlignment = Alignment.BottomCenter) {
+                Button(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp),
+                    onClick = { /*TODO*/ }) {
+                    Text(text = "SIGN UP")
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
                     Row {
-                        Text(text = "Already a user?",
+                        Text(
+                            text = "Already a user?",
                             fontSize = 16.sp,
-                            fontWeight = FontWeight.Light)
+                            fontWeight = FontWeight.Light
+                        )
                         Spacer(modifier = Modifier.width(6.dp))
-                        Text(text = "LOGIN",
+                        Text(
+                            text = "LOGIN",
                             fontSize = 16.sp,
                             fontWeight = FontWeight.Light,
                             color = MaterialTheme.colors.primary,
                             style = TextStyle(textDecoration = TextDecoration.Underline),
-                            modifier = Modifier.clickable( onClick = navigateLogin )
+                            modifier = Modifier.clickable(onClick = navigateLogin)
                         )
                     }
                 }
