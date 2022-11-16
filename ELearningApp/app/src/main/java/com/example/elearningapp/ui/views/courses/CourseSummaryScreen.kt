8d25332d4458a345957ff36f95174207ed7e3a07
Rasm-P <rasmus.pra@gmail.com>
2022-11-17 00:09:01 +0100

    Removed unused import

--- ELearningApp/app/src/main/java/com/example/elearningapp/ui/views/courses/CourseSummaryScreen.kt
+++ ELearningApp/app/src/main/java/com/example/elearningapp/ui/views/courses/CourseSummaryScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Flag
 import androidx.compose.material.icons.filled.School
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
