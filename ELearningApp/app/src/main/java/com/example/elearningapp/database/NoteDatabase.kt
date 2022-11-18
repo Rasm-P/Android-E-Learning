54be39639a82812fee9df17f3f2827abf318cb7e
Rasm-P <rasmus.pra@gmail.com>
2022-11-19 00:22:59 +0100

    Updated the NoteDatabase a little

--- ELearningApp/app/src/main/java/com/example/elearningapp/database/NoteDatabase.kt
+++ ELearningApp/app/src/main/java/com/example/elearningapp/database/NoteDatabase.kt
@@ -7,9 +7,12 @@ import com.example.elearningapp.common.DatabaseTypeConverters
 import com.example.elearningapp.database.dao.NoteDao
 import com.example.elearningapp.models.entities.NoteEntity
 
+//Built with inspiration from: https://github.com/HenrikPihl/retrofit_room/blob/feature/add-search/app/src/main/java/io/shortcut/dtucourceretrofit_room/datasource/AppDatabase.kt
+
 @Database(
     entities = [NoteEntity::class],
-    exportSchema = false,
+    autoMigrations = [],
+    exportSchema = true,
     version = 1)
 @TypeConverters(DatabaseTypeConverters::class)
 abstract class NoteDatabase : RoomDatabase() {
